0da0cc1576865aba39206c830cf2e55c35a646b3
Mati Joseph <crucialniccur@gmail.com>
2025-06-23 04:20:13 +0300

    Add Episode model to define episode attributes and relationships

---
+++ server/models/episode.py
@@ -0,0 +1,15 @@
+from server.app import db
+
+
+class Episode(db.Model):
+    __tablename__ = "episodes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship(
+        "Appearance", backref="episode", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<Episode #{self.number} - {self.date}>"
