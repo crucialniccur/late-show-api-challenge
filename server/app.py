bcdefa9d405729867228b7256c9af9d60b279081
Mati Joseph <crucialniccur@gmail.com>
2025-06-23 04:11:35 +0300

    Reorganize Pipfile packages and implement initial Flask app structure with User model

---
+++ server/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, make_response
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+app = Flask(__name__)
+app.config.from_prefixed_env()
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+api = Api(app)
+
+# import all models here
+
+
+class Home(Resource):
+    def get(self):
+
+        return {"message": "Hello, World!"}
+
+
+api.add_resource(Home, '/')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5555)
