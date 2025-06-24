dcbebddad78965dc5cd039f49c67269b8295180b
Mati Joseph <crucialniccur@gmail.com>
2025-06-24 23:21:17 +0300

    Refactor auth_controller.py import from extensions

--- server/controllers/auth_controller.py
+++ server/controllers/auth_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from server.models.user import User
-from server.config import db
+from server.extensions import db
 from flask_jwt_extended import create_access_token
 
 auth_bp = Blueprint('auth', __name__)
